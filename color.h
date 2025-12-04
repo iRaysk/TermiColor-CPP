085e1dfddd084da2096ddf04845c01b73d8ee3f7
Jessed <jessedmanuel123@gmail.com>
2025-12-04 00:20:42 -0400

    First version of color

---
+++ color.h
@@ -0,0 +1,50 @@
+#ifndef COLORS_H
+#define COLORS_H
+#include <iostream>
+#include <string>
+
+    //reset Colors
+const std::string reset = "\033[0m";
+
+    //Basic Colors
+const std::string red = "\033[0;31m";
+const std::string green = "\033[0;31m";
+const std::string yellow = "\033[0;33m";
+const std::string blue = "\033[0;34m";
+const std::string purple = "\033[0;35m";
+const std::string cyan = "\033[0;36m";
+const std::string white = "\033[0;37m";
+
+    //Background: 
+const std::string blackBG = "\033[0;40m";
+const std::string redBG = "\033[0;41m";
+const std::string greenBG = "\033[0;42m";
+const std::string yellowBG = "\033[0;43m";
+const std::string blueBG = "\033[0;44m";
+const std::string purpleBG = "\033[0;45m";
+const std::string cyanBG = "\033[0;46m";
+const std::string WhiteBG = "\033[0;47m";
+
+    //High Intensity Colors:
+const std::string bBlack = "\033[0;90m";
+const std::string bRed = "\033[0;91m";
+const std::string bGreen = "\033[0;92m";
+const std::string bYellow = "\033[0;93m";
+const std::string bBlue = "\033[0;94m";
+const std::string bPurple = "\033[0;95m";
+const std::string bCyan = "\033[0;96m";
+const std::string bWhite = "\033[0;97m";
+
+    //High Intensity Backgrounds:
+const std::string bBlackBG = "\033[0;100m";
+const std::string bRedBG = "\033[0;101m";
+const std::string bGreenBG = "\033[0;102m";
+const std::string bYellowBG = "\033[0;103m";
+const std::string bBlueBG = "\033[0;104m";
+const std::string bPurpleBG = "\033[0;105m";
+const std::string bCyanBG = "\033[0;106m";
+const std::string bWhiteBG = "\033[0;107m";
+
+
+
+#endif
---
+++ colors.cpp
@@ -0,0 +1,8 @@
+#include "color.h"
+using namespace std;
+
+int main(){
+    cout << red << "Hola Mundo!" << reset;
+
+    return 0;
+}
